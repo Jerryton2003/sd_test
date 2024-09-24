ac33f139f93ccf7a5d35c30ecacc17e5f132c5c5
Jerryton2003 <791051291@qq.com>
2024-09-24 20:53:04 +0800

    fix

--- get_utils.py
+++ get_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import tqdm
 #处理db文件
 import sqlite3
 #处理json文件
@@ -11,7 +12,7 @@ import glob
 n = 54
 results = []
 
-for i in range(n):
+for i in tqdm(range(n)):
     # perf文件夹路径,使用glob匹配到/PROF*路径
     dir_to_perf = glob.glob(f'sd_perf/test{i}/PROF*')[0]
     hbm_path = f'{dir_to_perf}/device_0/sqlite/hbm.db'
